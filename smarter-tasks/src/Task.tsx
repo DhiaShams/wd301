c6431bd9f6b5968e2d84d95227da60073116f4ff
DhiaShams <dhiadhilu123@gmail.com>
2025-05-21 19:41:47 +0530

    Level 4 submission: converted class based components to function based componenets

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,27 +1,30 @@
-import React from "react";
 import "./TaskCard.css";
 
 interface TaskProp {
-    title: string;
-    dueDate: string;
-    description: string;
-  }
-
-class Task extends React.Component<TaskProp> {
-    render() {
-    return (
-      <div className="TaskItem shadow-md border border-stone-700 max-w-sm rounded overflow-hidden">
-        <h2 className="text-base font-bold my-1 text-white">
-          {this.props.title}
-        </h2>
-        <p className="text-sm text-white">
-          Due Date: <span>{this.props.dueDate}</span>
-        </p>
-        <p className="text-sm text-white">
-          Description: <span>{this.props.description}</span>
-        </p>
-      </div>
-    );
-  }
+  title: string;
+  dueDate: string;
+  description: string;
+  deleteTask: () => void;
 }
+
+const Task = (props: TaskProp) => {
+  return (
+    <div className="TaskItem shadow-md border border-stone-700 max-w-sm rounded overflow-hidden">
+      <h2 className="text-base font-bold my-1 text-gray">{props.title}</h2>
+      <p className="text-sm text-gray">
+        Due Date: <span>{props.dueDate}</span>
+      </p>
+      <p className="text-sm text-gray">
+        Description: <span>{props.description}</span>
+      </p>
+      <button
+        className="deleteTaskButton m-2 p-2 bg-sky-300 hover:bg-sky-600 rounded-lg"
+        onClick={props.deleteTask}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
 export default Task;
--- smarter-tasks/src/TaskList.tsx
+++ smarter-tasks/src/TaskList.tsx
@@ -1,23 +1,23 @@
-import React from "react";
 import Task from "./Task";
 import type { TaskItem } from "./types";
 
 interface Props {
   tasks: TaskItem[];
+  handleDeleteTask: (id: number) => void;
 }
+// interface State {}
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-interface State {}
-class TaskList extends React.Component<Props, State> {
-  render() {
-    return this.props.tasks.map((task, idx) => (
-<Task
-        key={idx}
-        title={task.title}
-        dueDate={task.dueDate}
-        description={task.description}
-      />
-    ));
-  }
-}
+const TaskList = (props: Props) => {
+  const list = props.tasks.map((task, idx) => (
+    <Task
+      key={idx}
+      title={task.title}
+      description={task.description}
+      dueDate={task.dueDate}
+      deleteTask={() => props.handleDeleteTask(idx)}
+    />
+  ));
+  return <>{list}</>;
+};
+
 export default TaskList;
