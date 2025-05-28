de3dbb3a73b0834253bf86a59d2fed7eb1528eb9
DhiaShams <dhiadhilu123@gmail.com>
2025-05-28 15:44:07 +0530

    Added sign in page

--- smarter-tasks/src/components/Header.tsx
+++ smarter-tasks/src/components/Header.tsx
@@ -1,5 +1,5 @@
 const Header = () => {
-  return (
+   return (
     <nav className="bg-gray-800 py-4">
       <div className="mx-auto px-4">
         <div className="flex justify-between">
@@ -14,6 +14,11 @@ const Header = () => {
           <div className="flex items-center w-1/3 justify-center">
             <h2 className="text-white text-lg font-bold">Smarter Tasks</h2>
           </div>
+          <div className="flex items-center w-1/3 justify-end">
+            <a href="/signin" className="ml-6 text-gray-300 hover:text-white">
+             Signout
+            </a>
+         </div>
         </div>
       </div>
     </nav>
